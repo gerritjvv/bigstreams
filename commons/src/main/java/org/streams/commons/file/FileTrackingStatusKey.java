04f3a19b49d8b865f80bb6c1aceb47dccb9bcccb
gerritjvv <gerritjvv@e9027e87-718f-2365-ccfc-279084edcb2a>
2010-11-17 13:06:23 +0000

    adding FileTrackingStatusKey to support better handling of the FileTrackingStatys composite key
    git-svn-id: https://bigstreams.googlecode.com/svn/trunk@211 e9027e87-718f-2365-ccfc-279084edcb2a

---
+++ commons/src/main/java/org/streams/commons/file/FileTrackingStatusKey.java
@@ -0,0 +1,99 @@
+package org.streams.commons.file;
+
+import java.io.Serializable;
+
+
+/**
+ * Used to represent the composite key that FileTrackingStatus contains made up of : logType + agentName + fileName
+ *
+ */
+public class FileTrackingStatusKey implements Serializable, Comparable<FileTrackingStatusKey>{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	String agentName = null;
+	String logType = null;
+	String fileName = null;
+	
+	public FileTrackingStatusKey(){}
+	
+	public FileTrackingStatusKey(FileTrackingStatus status){
+		this.agentName = status.getAgentName();
+		this.logType = status.getLogType();
+		this.fileName = status.getFileName();
+	}
+
+	public String getAgentName() {
+		return agentName;
+	}
+
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+
+	public String getLogType() {
+		return logType;
+	}
+
+	public void setLogType(String logType) {
+		this.logType = logType;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+
+	public String getKey() {
+		return logType + agentName + fileName;
+	}
+
+	
+	
+	@Override
+	public int hashCode() {
+		String key = getKey();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FileTrackingStatusKey other = (FileTrackingStatusKey) obj;
+		String key = getKey();
+		String otherKey = other.getKey();
+		
+		if (key == null) {
+			if (otherKey != null)
+				return false;
+		} else if (!key.equals(otherKey))
+			return false;
+		return true;
+	}
+
+	
+	
+	@Override
+	public int compareTo(FileTrackingStatusKey f) {
+		return getKey().compareTo(f.getKey());
+	}
+
+
+	
+}
