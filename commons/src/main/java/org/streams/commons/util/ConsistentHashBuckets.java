dbcda611629dcc0686c75259960cf9e57da919c7
gerritjvv <gerritjvv@e9027e87-718f-2365-ccfc-279084edcb2a>
2011-10-10 09:42:17 +0000

    fixed bucket problem
    git-svn-id: https://bigstreams.googlecode.com/svn/trunk@513 e9027e87-718f-2365-ccfc-279084edcb2a

--- commons/src/main/java/org/streams/commons/util/ConsistentHashBuckets.java
+++ commons/src/main/java/org/streams/commons/util/ConsistentHashBuckets.java
@@ -1,5 +1,6 @@
 package org.streams.commons.util;
 
+import java.util.NoSuchElementException;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
@@ -8,23 +9,24 @@ import java.util.TreeSet;
 /**
  * 
  * Applies a consistent hash bucketing scheme.<br/>
- * I.e. N buckets are created e.g. 1 - 512.  <br/>
- * Key presented to the methods of this instance are consistently assigned a bucket.
+ * I.e. N buckets are created e.g. 1 - 512. <br/>
+ * Key presented to the methods of this instance are consistently assigned a
+ * bucket.
  */
 public class ConsistentHashBuckets {
 
 	int totalBuckets = 10;
 	int keyRange = Integer.MAX_VALUE;
-	
-	final SortedMap<Integer,SortedSet<Integer>> buckets;
-	
+
+	final SortedMap<Integer, SortedSet<Integer>> buckets;
+
 	/**
 	 * Create 256 buckets
 	 */
 	public ConsistentHashBuckets() {
 		buckets = createBuckets();
 	}
-	
+
 	/**
 	 * 
 	 * @param totalBuckets
@@ -34,6 +36,7 @@ public class ConsistentHashBuckets {
 		this.totalBuckets = totalBuckets;
 		buckets = createBuckets();
 	}
+
 	/**
 	 * 
 	 * @param totalBuckets
@@ -45,49 +48,60 @@ public class ConsistentHashBuckets {
 		buckets = createBuckets();
 	}
 
-	private final SortedMap<Integer,SortedSet<Integer>>createBuckets(){
-		int val = (int)(keyRange / totalBuckets);
-		
-		SortedMap<Integer,SortedSet<Integer>> bucketMap = new TreeMap<Integer, SortedSet<Integer>>();
-		
-		
-		
+	private final SortedMap<Integer, SortedSet<Integer>> createBuckets() {
+		int val = (int) (keyRange / totalBuckets);
+
+		SortedMap<Integer, SortedSet<Integer>> bucketMap = new TreeMap<Integer, SortedSet<Integer>>();
+
 		int space = val;
-		for(int i = 0; i < totalBuckets; i++){
+		for (int i = 0; i < totalBuckets; i++) {
 			int hashcode = new Integer(space).hashCode();
-			if(hashcode < 0) hashcode *= -1;
-			
+			if (hashcode < 0)
+				hashcode *= -1;
+
 			SortedSet<Integer> set = new TreeSet<Integer>();
-			
+
 			int innerBucketTotal = 512;
-			int innerVal = hashcode/innerBucketTotal;
+			int innerVal = hashcode / innerBucketTotal;
 			int innerSpace = innerVal;
-			for(int a = 0; a < innerBucketTotal; a++){
+			for (int a = 0; a < innerBucketTotal; a++) {
 				int innerhashcode = new Integer(innerSpace).hashCode();
-				if(innerhashcode < 0) innerhashcode *= -1;
+				if (innerhashcode < 0)
+					innerhashcode *= -1;
 				set.add(innerhashcode);
 				innerSpace += innerVal;
 			}
-			
+
 			bucketMap.put(hashcode, set);
 			space += val;
 		}
-		
-		
+
 		return bucketMap;
 	}
 
 	/**
 	 * For the key, consistently return the same bucket.
+	 * 
 	 * @param key
 	 * @return
 	 */
-	public Integer getBucket(String key){
+	public Integer getBucket(String key) {
 		int hashcode = key.hashCode();
-		if(hashcode < 0) hashcode *= -1;
-		
+		if (hashcode < 0)
+			hashcode *= -1;
+
 		Integer mapKey = buckets.tailMap(hashcode).firstKey();
-		return buckets.get(mapKey).tailSet(hashcode).first();
+
+		try {
+			return buckets.get(mapKey).tailSet(hashcode).first();
+		} catch (NoSuchElementException noel) {
+			try {
+				hashcode *= -1;
+				return buckets.get(mapKey).tailSet(hashcode).first();
+			} catch (NoSuchElementException noel1) {
+				return 1;
+			}
+		}
 	}
-	
+
 }
--- commons/src/test/java/org/streams/test/commons/util/ConsistentHashBucketsTest.java
+++ commons/src/test/java/org/streams/test/commons/util/ConsistentHashBucketsTest.java
@@ -3,6 +3,7 @@ package org.streams.test.commons.util;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,57 +14,78 @@ import org.streams.commons.util.ConsistentHashBuckets;
 
 public class ConsistentHashBucketsTest {
 
+	@Test
+	public void testNoElementFound() {
+		String str = "/log/adserver/gpb/ads_queue/adsdeliveries_ads_queue_93.184.97.172_8506.2011-10-08-00.25_9_770330.txt";
+
+		List<String> types = Arrays.asList("ads", "noads", "clicks");
+
+		for (String type : types) {
+
+			String key = "adserver32.zestadz.com" + type
+					+ str.replace('/', '_');
+
+			ConsistentHashBuckets buckets = new ConsistentHashBuckets();
+			Integer bucket = buckets.getBucket(key);
+			assertTrue(bucket != 1);
+		}
+
+	}
+
 	/**
 	 * Tests the hash spread against a known value.<br/>
-	 * i.e. for the fixed values presented the maximum items in a bucket cannot exceed more than N,
-	 * where N is a known value e.g 100.</br/>
+	 * i.e. for the fixed values presented the maximum items in a bucket cannot
+	 * exceed more than N, where N is a known value e.g 100.</br/>
 	 */
 	@Test
-	public void testHashSpread(){
-		
-		//create file names with dates and hours for 1 month
+	public void testHashSpread() {
+
+		// create file names with dates and hours for 1 month
 		List<String> fileNames = new ArrayList<String>();
-		for(int months = 0; months < 12; months++){
-		for(int days =1; days < 31; days++){
-			for(int hours = 0; hours < 24; hours++){
-				fileNames.add("myfile_ "  + "2011-" + months + "-" + days + "-" + hours + ".txt");
-				fileNames.add("a_ "  + "2011-" + months + "-" + days + "-" + hours + ".txt");
-				fileNames.add("z_ "  + "2011-" + months + "-" + days + "-" + hours + ".txt");
-				fileNames.add("e_ "  + "2011-" + months + "-" + days + "-" + hours + ".txt");
+		for (int months = 0; months < 12; months++) {
+			for (int days = 1; days < 31; days++) {
+				for (int hours = 0; hours < 24; hours++) {
+					fileNames.add("myfile_ " + "2011-" + months + "-" + days
+							+ "-" + hours + ".txt");
+					fileNames.add("a_ " + "2011-" + months + "-" + days + "-"
+							+ hours + ".txt");
+					fileNames.add("z_ " + "2011-" + months + "-" + days + "-"
+							+ hours + ".txt");
+					fileNames.add("e_ " + "2011-" + months + "-" + days + "-"
+							+ hours + ".txt");
+				}
 			}
 		}
-		}
-		
+
 		ConsistentHashBuckets buckets = new ConsistentHashBuckets();
 		Map<Integer, AtomicInteger> bucketMap = new HashMap<Integer, AtomicInteger>();
-		
+
 		int hashMax = 0;
 		int hashMin = Integer.MAX_VALUE;
-		
-		for(String fileName : fileNames){
+
+		for (String fileName : fileNames) {
 			int hash = fileName.hashCode();
 			hashMax = Math.max(hashMax, hash);
 			hashMin = Math.min(hashMin, hash);
 			Integer bucket = buckets.getBucket(fileName);
 			AtomicInteger counts = bucketMap.get(bucket);
-			if(counts == null){
+			if (counts == null) {
 				counts = new AtomicInteger(1);
 				bucketMap.put(bucket, counts);
-			}else{
+			} else {
 				counts.incrementAndGet();
 			}
-			
+
 		}
-		
-		
+
 		int max = 0;
-		for(Integer key : bucketMap.keySet()){
+		for (Integer key : bucketMap.keySet()) {
 			max = Math.max(max, bucketMap.get(key).get());
 			System.out.println(key + ": " + bucketMap.get(key).get());
 		}
-		
+
 		System.out.println("Max: " + max);
 		assertTrue(max < 100);
 	}
-	
+
 }
