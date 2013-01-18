a629f5bd4e4c3590a326faf99a1c8ad0cafb7129
gerritjvv <gerritjvv@e9027e87-718f-2365-ccfc-279084edcb2a>
2013-01-18 14:35:15 +0000

    cut down on logging from replay
    git-svn-id: https://bigstreams.googlecode.com/svn/trunk@606 e9027e87-718f-2365-ccfc-279084edcb2a

--- kafka-collector/src/main/scala/org/streams/kafkacol/collector/LogFileReplayCheck.scala
+++ kafka-collector/src/main/scala/org/streams/kafkacol/collector/LogFileReplayCheck.scala
@@ -11,13 +11,16 @@ import org.apache.log4j.Logger
 class LogFileReplayCheck(logResource: FileLogResource) {
 
   val logger = Logger.getLogger(classOf[LogFileReplayCheck])
-
+  var i = 0
+  
   def check(baseDir: File) {
     logger.info("Starting replay check for directory: " + baseDir.getAbsolutePath())
     WalReplay.replay(baseDir, {
 
       (topic, date, msg) =>
-        logger.warn("Replaying Log for: topic=" + topic + " date=" + date)
+        if(i % 100000 == 0)
+        	logger.warn("Replaying Log for: topic=" + topic + " date=" + date + " records replayed: " + i)
+        i = i + 1
         //send synchronously with a 10 second timeout
         logResource.get(topic) ! (date, msg)
     }, true)
