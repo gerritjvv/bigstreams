4c623c257e63547e3eda447a0b48f7634374e297
gerritjvv <gerritjvv@e9027e87-718f-2365-ccfc-279084edcb2a>
2013-07-01 13:34:51 +0000

    Fixes #79
    git-svn-id: https://bigstreams.googlecode.com/svn/trunk@648 e9027e87-718f-2365-ccfc-279084edcb2a

--- kafka-collector/src/main/resources/conf/collector-env.sh
+++ kafka-collector/src/main/resources/conf/collector-env.sh
@@ -6,5 +6,5 @@
 ## JAVA OPTIONS
 export JAVA_OPTS="-Djava.library.path=/opt/hadoopgpl/native/Linux-amd64-64/"
 
-export JAVA_MEM_OPTS="-Xmx1024m -Xss160k -XX:MaxDirectMemorySize=1024M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+DisableExplicitGC"
+export JAVA_MEM_OPTS="-Xmx1024m -XX:MaxDirectMemorySize=1024M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+DisableExplicitGC"
 
